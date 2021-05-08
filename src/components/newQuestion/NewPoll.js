0984f731703cc703cffebd4cb4474e8e728b7e3b
Ahmad Ayman <mongez.me@gmail.com>
2021-05-08 03:33:29 +0200

    fix styled component inputs loses focus by decouple styles from the component render

--- src/components/newQuestion/NewPoll.js
+++ src/components/newQuestion/NewPoll.js
@@ -1,24 +1,26 @@
 import React from "react";
 import styled from "@emotion/styled";
 
-const NewPoll = ({ name, text }) => {
-  const Input = styled.input`
-    margin: 0.7rem 0;
-    width: 90%;
-    border: none;
-    height: 2rem;
-    outline: none;
-    font-size: 1.1rem;
-  `;
-
+const Input = styled.input`
+  margin: 0.7rem 0;
+  width: 90%;
+  border: none;
+  height: 2rem;
+  outline: none;
+  font-size: 1.1rem;
+`;
+const NewPoll = ({ name, text, onChange, value, ref1, onClick, index }) => {
   return (
     <li style={{ color: "#7E8594" }}>
       <Input
         type="text"
         id={name}
         name={name}
-        value={null}
+        value={value}
         placeholder={`Add ${name}`}
+        ref={(el) => ref1(el)}
+        onChange={onChange}
+        onClick={() => onClick(index)}
       />
     </li>
   );
