c18ef80ceda9ceb7d61e4740245b1558776d084e
Ahmad Ayman <mongez.me@gmail.com>
2021-05-08 18:06:21 +0200

    questions details show correct question state

--- src/components/questionDetails/QuestionDetails.js
+++ src/components/questionDetails/QuestionDetails.js
@@ -1,32 +1,35 @@
-import React, { useImperativeHandle } from "react";
 import { connect } from "react-redux";
-import { useParams, useRouteMatch } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 import Header from "../question/Header";
 import AnsweredFooter from "../answered/Footer";
-import UnAnsweredFooter from "../latest/Footer";
 
 import AnsweredContent from "../answered/Content";
 import UnAnsweredContent from "../latest/Content";
 
 import { Container, RoundedContainer } from "../CustomStyles";
 
-const QuestionDetails = ({ questions, users, authedUser }) => {
+const QuestionDetails = ({ questions, users, authedUserId }) => {
   const { id } = useParams();
   const question = questions[id];
   const voters = question.optionOne.votes.concat(question.optionTwo.votes);
   const votersAvatars = voters.map((voter) => users[voter].avatarURL);
 
   console.log(votersAvatars);
+
+  const isAnswerd = users[authedUserId].answers[question.id] !== undefined;
+
   return (
-    <Container centered aligncenter>
+    <Container centered aligncenter column top="2rem">
+      {isAnswerd ? <h3>You answered this</h3> : <h3>Answer this question</h3>}
+
       <RoundedContainer>
         <Header
           avatarURL={users[question.author].avatarURL}
           author={users[question.author].name}
           timestamp={question.timestamp}
         />
-        {users[authedUser.id].answers[question.id] ? (
+        {isAnswerd ? (
           <>
             <AnsweredContent question={question} />
             <AnsweredFooter votersAvatars={votersAvatars} />
@@ -47,7 +50,7 @@ const QuestionDetails = ({ questions, users, authedUser }) => {
 const mapStateToProps = (state) => ({
   questions: state.questions,
   users: state.users,
-  authedUser: state.authedUser,
+  authedUserId: state.authedUser.id,
 });
 
 export default connect(mapStateToProps)(QuestionDetails);
